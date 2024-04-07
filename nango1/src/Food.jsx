0835f9101a754b635d7d34a69ddb25c4f939bd16
S-okubomy <okubo1504@gmail.com>
2024-04-07 18:44:30 +0900

    updated

--- nango1/src/Food.jsx
+++ nango1/src/Food.jsx
@@ -11,12 +11,12 @@ class Food extends React.Component {
         <section>
           <h2 className="title"><span style={{ fontSize: "15px", fontWeight: "bold" }} >■お食事■</span></h2>
           <ul className="post">
-            <li><img src="https://nango-img.s3-ap-northeast-1.amazonaws.com/food1.jpg" width="300" /><span style={{ fontSize: "20px", fontWeight: "bold" }} >お食事</span><br/>
+            <li><img src="/static/hp_nango/images/zoni1.jpg" width="300" /><span style={{ fontSize: "20px", fontWeight: "bold" }} >お食事</span><br/>
               <p>
                 お料理は当日のおすすめメニューのみとなります。
               </p>
             </li>
-            <li><img src="https://nango-img.s3-ap-northeast-1.amazonaws.com/food2.jpg" width="300" /><span style={{ fontSize: "20px", fontWeight: "bold" }} >一品料理</span><br/>
+            <li><img src="/static/hp_nango/images/tuku1.jpg" width="300" /><span style={{ fontSize: "20px", fontWeight: "bold" }} >一品料理</span><br/>
               <p>
                 冷奴　　　　　　　　　　　400円<br/>
                 枝豆　　　　　　　　　　　400円<br/>
